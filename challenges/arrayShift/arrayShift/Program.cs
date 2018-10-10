2f44ab54e7c2a1ea3b4a1b141222ac6887280c96
Richard J <richjim89@gmail.com>
2018-10-09 18:02:00 -0700

    created method implementation

--- challenges/arrayShift/arrayShift/Program.cs
+++ challenges/arrayShift/arrayShift/Program.cs
@@ -6,7 +6,26 @@ namespace arrayShift
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+        }
+
+        static int[] ArrayShift(int[] arr, int num)
+        {
+            int[] newArr = new int[arr.Length + 1];
+            decimal halfOfArr = newArr.Length / 2;
+            int midIdx = Convert.ToInt32(Math.Ceiling(halfOfArr));
+            int counter = 0;
+            for (int i = 0; i < newArr.Length; i++)
+            {
+                if (i == midIdx)
+                    newArr[i] = num;
+                else
+                {
+                    newArr[i] = arr[counter];
+                    counter++;
+                }
+            }
+            return newArr;
         }
     }
 }
