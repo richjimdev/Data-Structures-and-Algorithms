a410cfd08e31c71bcfd84454a66e786a731e5d39
Richard J <richjim89@gmail.com>
2018-10-10 17:33:56 -0700

    test 2

--- challenges/BinarySearch/BinaryTest/UnitTest1.cs
+++ challenges/BinarySearch/BinaryTest/UnitTest1.cs
@@ -14,5 +14,14 @@ namespace BinaryTest
             int value = 15;
             Assert.Equal(2, BinarySearchMethod(inputArray, value));
         }
+
+        [Fact]
+        public void MethodReturnsNegativeIndexForInputNotFound()
+        {
+            int[] inputArray = { 4, 8, 15, 16, 23, 42 };
+            int value = 25;
+            Assert.Equal(-1, BinarySearchMethod(inputArray, value));
+        }
+
     }
 }
