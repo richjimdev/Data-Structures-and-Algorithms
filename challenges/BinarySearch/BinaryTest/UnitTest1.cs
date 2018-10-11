d96af4b331e4583d38ce64eac3402d5e39efc5bd
Richard J <richjim89@gmail.com>
2018-10-10 17:26:39 -0700

    added code for implementation

---
+++ challenges/BinarySearch/BinaryTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using BinarySearch;
+using static BinarySearch.Program;
+
+namespace BinaryTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
