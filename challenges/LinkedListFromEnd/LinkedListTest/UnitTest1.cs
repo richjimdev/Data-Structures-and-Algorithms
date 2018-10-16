aba0a84f65475dad16c36e0e6e9a818cbd842f1c
Richard J <richjim89@gmail.com>
2018-10-16 15:24:04 -0700

    testing for exception throws

--- challenges/LinkedListFromEnd/LinkedListTest/UnitTest1.cs
+++ challenges/LinkedListFromEnd/LinkedListTest/UnitTest1.cs
@@ -16,7 +16,7 @@ namespace LinkedListTest
         [InlineData(4, "c")]
         [InlineData(5, "b")]
         [InlineData(6, "a")]
-        public void Test1(int input, object expected)
+        public void InputReturnsCorrectOutput(int input, object expected)
         {
             //arrange
             Node node1 = new Node("a");
@@ -35,8 +35,28 @@ namespace LinkedListTest
 
             //Assert
             Assert.Equal(expected, FindKthElement(list1, input));
-            //Assert.Equal(expected, FindKthElement(list1, input));
+        }
 
+        [Fact]
+        public void InputOutsideOfRangeReturnsException()
+        {
+            //arrange
+            Node node1 = new Node("a");
+            Node node2 = new Node("b");
+            Node node3 = new Node("c");
+            Node node4 = new Node("d");
+            Node node5 = new Node("e");
+            Node node6 = new Node("f");
+            Node node7 = new Node("g");
+            Node[] nodeArr = { node6, node5, node4, node3, node2, node1 };
+
+            LList list1 = new LList(node7);
+
+            foreach (Node node in nodeArr)
+                list1.Add(node);
+
+            //Assert
+            Assert.Throws<Exception>(() => FindKthElement(list1, 9));
         }
     }
 }
