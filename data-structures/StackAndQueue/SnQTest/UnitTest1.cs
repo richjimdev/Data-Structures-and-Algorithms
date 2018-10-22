cbb76c2893277b81ac1e5e0dee01fa62019429ed
Richard J <richjim89@gmail.com>
2018-10-22 11:11:31 -0700

    tests complete

--- data-structures/StackAndQueue/SnQTest/UnitTest1.cs
+++ data-structures/StackAndQueue/SnQTest/UnitTest1.cs
@@ -7,6 +7,9 @@ namespace SnQTest
 {
     public class UnitTest1
     {
+        /// <summary>
+        /// Checking that a stack can be created
+        /// </summary>
         [Fact]
         public void CreatingStackAddsNodeAtStackTop()
         {
@@ -18,6 +21,9 @@ namespace SnQTest
             Assert.Equal(1, myStack.Top.Value);
         }
 
+        /// <summary>
+        /// Checking that a node can be pushed to stack
+        /// </summary>
         [Fact]
         public void StackPushNodeAddsNodeToTopOfStack()
         {
@@ -32,6 +38,9 @@ namespace SnQTest
             Assert.Equal(2, myStack.Top.Value);
         }
 
+        /// <summary>
+        /// Checking that Peek() works
+        /// </summary>
         [Fact]
         public void PeekShowsTopNodeOfStack()
         {
@@ -46,6 +55,9 @@ namespace SnQTest
             Assert.Equal(2, myStack.Peek().Value);
         }
 
+        /// <summary>
+        /// Checking that Pop() gives back the Node's value
+        /// </summary>
         [Fact]
         public void PopGrabsTopNodeFromStackAndGivesBackItsValue()
         {
@@ -60,6 +72,9 @@ namespace SnQTest
             Assert.Equal(2, myStack.Pop().Value);
         }
 
+        /// <summary>
+        /// Checking that Pop removes the node from the stack
+        /// </summary>
         [Fact]
         public void PopRemovesTopNodeFromStack()
         {
@@ -73,6 +88,58 @@ namespace SnQTest
 
             //Assert
             Assert.Equal(1, myStack.Top.Value);
+        }
+
+        /// <summary>
+        /// Checking that Peek works for queues
+        /// </summary>
+        [Fact]
+        public void QueuePeekShowsFrontNode()
+        {
+            //Arrange
+            Node node1 = new Node(1);
+            Node node2 = new Node(2);
+
+            Queue myQueue = new Queue(node1);
+            myQueue.Enqueue(node2);
+
+            //Assert
+            Assert.Equal(1, myQueue.Peek().Value);
+        }
+
+        /// <summary>
+        /// Checking that enqueue adds node to queue
+        /// </summary>
+        [Fact]
+        public void EnqueueAddsNodeToRear()
+        {
+            //Arrange
+            Node node1 = new Node(1);
+            Node node2 = new Node(2);
+
+            Queue myQueue = new Queue(node1);
+            myQueue.Enqueue(node2);
+
+            //Assert
+            Assert.Equal(2, myQueue.Rear.Value);
+        }
+
+        /// <summary>
+        /// Checking that dequeue removes node from queue
+        /// </summary>
+        [Fact]
+        public void DequeueRemovesFrontNode()
+        {
+            //Arrange
+            Node node1 = new Node(1);
+            Node node2 = new Node(2);
+
+            Queue myQueue = new Queue(node1);
+            myQueue.Enqueue(node2);
+            myQueue.Dequeue();
+
+            //Assert
+            Assert.NotEqual(1, myQueue.Front.Value);
         }
     }
 }
