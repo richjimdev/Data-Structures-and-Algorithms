a8f340c4eddc9f10e9eb32ffb357435b4b0c0e88
Richard J <richjim89@gmail.com>
2018-10-23 11:00:44 -0700

    Instantiated on Program

--- challenges/QueueWithStacks/QueueWithStacks/Program.cs
+++ challenges/QueueWithStacks/QueueWithStacks/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using QueueWithStacks.Classes;
+using StackAndQueue.Classes;
+using System;
 
 namespace QueueWithStacks
 {
@@ -6,7 +8,30 @@ namespace QueueWithStacks
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Node node1 = new Node(1);
+            Node node2 = new Node(2);
+            Node node3 = new Node(3);
+            Node node4 = new Node(4);
+
+            Stack stackOne = new Stack(null);
+                
+            Stack stackTwo = new Stack(null);
+            
+            QueueStack myQueue = new QueueStack(stackOne, stackTwo);
+
+            Console.WriteLine($"Enqueue: {node1.Value}");
+            myQueue.Enqueue(node1);
+            Console.WriteLine($"Enqueue: {node2.Value}");
+            myQueue.Enqueue(node2);
+            Console.WriteLine($"Enqueue: {node3.Value}");
+            myQueue.Enqueue(node3);
+            Console.WriteLine($"Enqueue: {node4.Value}");
+            myQueue.Enqueue(node4);
+
+            Console.WriteLine($"Deque: {myQueue.Dequeue().Value}");
+            Console.WriteLine($"Deque: {myQueue.Dequeue().Value}");
+            Console.WriteLine($"Deque: {myQueue.Dequeue().Value}");
+            Console.WriteLine($"Deque: {myQueue.Dequeue().Value}");
         }
     }
 }
