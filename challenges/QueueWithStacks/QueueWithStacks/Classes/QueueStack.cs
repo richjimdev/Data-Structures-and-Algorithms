d11a25edf5c5eaed06efae0f41255c148b761914
Richard J <richjim89@gmail.com>
2018-10-23 10:49:23 -0700

    Implementation

---
+++ challenges/QueueWithStacks/QueueWithStacks/Classes/QueueStack.cs
@@ -0,0 +1,39 @@
+﻿using StackAndQueue.Classes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueueWithStacks.Classes
+{
+    public class QueueStack
+    {
+        public Stack A { get; set; }
+        public Stack B { get; set; }
+
+        public void Enqueue(Node node)
+        {
+            A.Push(node);
+        }
+
+        public Node Dequeue()
+        {
+            while (A.Top.Next != null)
+            {
+                B.Push(A.Pop());
+            }
+            Node temp = A.Pop();
+            while (B.Top != null)
+            {
+                A.Push(B.Pop());
+            }
+
+            return temp;
+        }
+
+        public QueueStack(Stack one, Stack two)
+        {
+            A = one;
+            B = two;
+        }
+    }
+}
