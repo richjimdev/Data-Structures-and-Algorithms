7588e37a3fd24bd02f5693e032972468c732bf96
Richard J <richjim89@gmail.com>
2018-10-23 22:29:28 -0700

    Dequeue method

--- challenges/FIFOAnimalShelter/FIFOAnimalShelter/Classes/AnimalShelter.cs
+++ challenges/FIFOAnimalShelter/FIFOAnimalShelter/Classes/AnimalShelter.cs
@@ -8,11 +8,13 @@ namespace FIFOAnimalShelter.Classes
     {
         public Animal Front { get; set; }
         public Animal Rear { get; set; }
+        public int Count { get; set; } = 0;
 
         public AnimalShelter(Animal node)
         {
             Front = node;
             Rear = node;
+            Count++;
         }
 
         public Animal Peek()
@@ -24,6 +26,46 @@ namespace FIFOAnimalShelter.Classes
         {
             Rear.Next = node;
             Rear = node;
+            Count++;
+        }
+
+        public Animal Dequeue(string input)
+        {
+            if (input != "dog" || input != "cat")
+                return Dequeue();
+            Animal temp = Front;
+            if (input == "dog")
+            {
+                if (Front is Dog)
+                    return Dequeue();
+
+                while (!(Front is Dog))
+                {
+                    Enqueue(Dequeue());
+                    if (Front == temp)
+                        return null;
+                }
+            }
+            if (input == "cat")
+            {
+                if (Front is Cat)
+                    return Dequeue();
+
+                while (!(Front is Cat))
+                {
+                    Enqueue(Dequeue());
+                    if (Front == temp)
+                        return null;
+                }
+            }
+
+            Animal result = Dequeue();
+            while (Front != temp)
+            {
+                Enqueue(Dequeue());
+            }
+
+            return result;
         }
 
         public Animal Dequeue()
@@ -31,6 +73,7 @@ namespace FIFOAnimalShelter.Classes
             Animal temp = Front;
             Front = Front.Next;
             temp.Next = null;
+            Count--;
             return temp;
         }
     }
