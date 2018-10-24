021f4e714244be69a1d88b7c40d9f8f1441f04f2
Richard J <richjim89@gmail.com>
2018-10-24 14:13:29 -0700

    implementation

---
+++ challenges/MultiBracketValidation/MultiBracketValidation/Program.cs
@@ -0,0 +1,43 @@
+﻿using StackAndQueue.Classes;
+using System;
+
+namespace MultiBracketValidation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+        public static bool MultiBracketValidation(string input)
+        {
+            Stack holder = new Stack(null);
+
+            foreach (char x in input)
+            {
+                string letter = x.ToString();
+
+                if (letter == "(")
+                    holder.Push(new Node(")"));
+                if (letter == "{")
+                    holder.Push(new Node("}"));
+                if (letter == "[")
+                    holder.Push(new Node("]"));
+
+                if (letter == ")" ||
+                    letter == "}" ||
+                    letter == "]")
+                {
+                    if (holder.Top == null)
+                        return false;
+                    if (letter != holder.Pop().Value.ToString())
+                        return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
