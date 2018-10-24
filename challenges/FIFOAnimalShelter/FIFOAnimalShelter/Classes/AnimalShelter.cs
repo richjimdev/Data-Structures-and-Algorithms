904b48e83fe4714a74ffce233194bf0b6c6fd570
Richard J <richjim89@gmail.com>
2018-10-23 21:59:13 -0700

    Animal Shelter Class

---
+++ challenges/FIFOAnimalShelter/FIFOAnimalShelter/Classes/AnimalShelter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FIFOAnimalShelter.Classes
+{
+    public class AnimalShelter
+    {
+        public Animal Front { get; set; }
+        public Animal Rear { get; set; }
+
+        public AnimalShelter(Animal node)
+        {
+            Front = node;
+            Rear = node;
+        }
+
+        public Animal Peek()
+        {
+            return Front;
+        }
+
+        public void Enqueue(Animal node)
+        {
+            Rear.Next = node;
+            Rear = node;
+        }
+
+        public Animal Dequeue()
+        {
+            Animal temp = Front;
+            Front = Front.Next;
+            temp.Next = null;
+            return temp;
+        }
+    }
+}
