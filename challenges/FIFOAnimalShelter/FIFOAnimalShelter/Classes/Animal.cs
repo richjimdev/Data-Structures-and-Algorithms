7db8a90ae579f3e210bd669827e119640df1fe10
Richard J <richjim89@gmail.com>
2018-10-23 21:57:08 -0700

    created classes

---
+++ challenges/FIFOAnimalShelter/FIFOAnimalShelter/Classes/Animal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FIFOAnimalShelter.Classes
+{
+    public abstract class Animal
+    {
+        public object Value { get; set; }
+        public Animal Next { get; set; }
+    }
+}
---
+++ challenges/FIFOAnimalShelter/FIFOAnimalShelter/Classes/Dog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FIFOAnimalShelter.Classes
+{
+    public class Dog : Animal
+    {
+        Dog(object value)
+        {
+            Value = value;
+        }
+    }
+}
