270af0e56d5d990b9cf91fbf0b7e272474a144a4
Richard J <richjim89@gmail.com>
2018-10-29 11:02:57 -0700

    binary tree preorder traversal

---
+++ data-structures/Tree/Tree/Classes/BinaryTree.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tree.Classes
+{
+    public class BinaryTree
+    {
+        public Node Root { get; set; }
+
+        public BinaryTree(Node root)
+        {
+            Root = root;
+        }
+
+
+        List<Node> result = new List<Node>();
+
+        public List<Node> PreOrder(Node root)
+        {
+            if (root.Left != null)
+                PreOrder(root.Left);
+            result.Add(root);
+            if (root.Right != null)
+                PreOrder(root.Right);
+
+            return result;
+        }
+    }
+}
---
+++ data-structures/Tree/Tree/Classes/Node.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tree.Classes
+{
+    public class Node
+    {
+        public object Value { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+    }
+}
