b7001e8211bd268bf04f86bad6a665a5c4292a9f
richjimdev <richjimdev@gmail.com>
2018-10-28 20:00:08 -0700

    implemented code

---
+++ challenges/EeneyMeeney/EeneyMeeney/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EeneyMeeney
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string> { "Richard", "Danul", "Allisa", "Kendra", "Suzzie" };
+
+            MineyMo(names, 3);
+        }
+
+        public static string MineyMo(List<string> names, int num)
+        {
+            int count = names.Count;
+
+            Queue<string> nameQueue = new Queue<string>(names);
+
+            while (count > 1)
+            {
+                for (int i = 0; i < num - 1; i++)
+                {
+                    nameQueue.Enqueue(nameQueue.Dequeue());
+                }
+                Console.WriteLine($"Eeney Meeney Miney Mo.... {nameQueue.Dequeue()} is out!");
+                count--;
+            }
+
+            Console.WriteLine($"{nameQueue.Dequeue()} wins!");
+            return "lol";
+        }
+    }
+}
