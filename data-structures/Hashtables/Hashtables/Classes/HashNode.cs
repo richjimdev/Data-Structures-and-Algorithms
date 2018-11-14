5c5a4063e67cb03d3a07904f1247c2383d004e84
richjimdev <richjimdev@gmail.com>
2018-11-13 21:02:58 -0800

    created node for hashtable

---
+++ data-structures/Hashtables/Hashtables/Classes/HashNode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hashtables.Classes
+{
+    public class HashNode
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+
+        /// <summary>
+        /// Building a node requires a value and key
+        /// </summary>
+        /// <param name="key">Key for node</param>
+        /// <param name="value">Value for node</param>
+        public HashNode(string key, string value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+}
