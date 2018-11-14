5e7f69d35b58fe154223d6c2d5763402b5e1f114
richjimdev <richjimdev@gmail.com>
2018-11-13 23:23:09 -0800

    instantiation

--- data-structures/Hashtables/Hashtables/Program.cs
+++ data-structures/Hashtables/Hashtables/Program.cs
@@ -7,12 +7,27 @@ namespace Hashtables
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
+            //Instantiating
             Hashtable hs = new Hashtable();
 
-            Console.WriteLine(hs.GetHashKey("Dog"));
-            Console.WriteLine(hs.GetHashKey("Dogg"));
+            //Adding
+            hs.Add("Greeting", "Hi");
+            hs.Add("Name", "Richard");
+            hs.Add("Animal", "Dog");
+
+            //Forcing a collision
+            hs.Add("Video Game", "Mario 64");
+            hs.Add("Aged Movie", "Jaws");
+
+            //proving all added keys exist
+            Console.WriteLine(hs.Contains("Greeting"));
+            Console.WriteLine(hs.Contains("Name"));
+            Console.WriteLine(hs.Contains("Animal"));
+
+            Console.WriteLine(hs.Contains("Video Game"));
+            Console.WriteLine(hs.Contains("Aged Movie"));
+
+            Console.WriteLine(hs.Contains("Something Else"));
         }
     }
 }
