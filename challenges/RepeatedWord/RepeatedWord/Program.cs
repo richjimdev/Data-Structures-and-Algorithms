3b665af55d7c4880ed109d00fb216ca318276345
Richard J <richjim89@gmail.com>
2018-11-15 11:18:39 -0800

    Implementation

---
+++ challenges/RepeatedWord/RepeatedWord/Program.cs
@@ -0,0 +1,30 @@
+﻿using Hashtables.Classes;
+using System;
+
+namespace RepeatedWord
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static string Repeated(string input)
+        {
+            string[] inputArr = input.ToLower().Split(" ");
+
+            Hashtable ht = new Hashtable();
+
+            foreach (string word in inputArr)
+            {
+                if (ht.Contains(word))
+                    return word;
+                else
+                    ht.Add(word, word);
+            }
+
+            return "NO DUPLICATES";
+        }
+    }
+}
