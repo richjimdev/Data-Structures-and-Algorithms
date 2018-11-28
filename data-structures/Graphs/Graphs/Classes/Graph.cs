23934803e8fb3cb937e902a0c745d4a2e6eb177b
Richard J <richjim89@gmail.com>
2018-11-28 13:11:41 -0800

    graph & ctor

---
+++ data-structures/Graphs/Graphs/Classes/Graph.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graphs.Classes
+{
+    public class Graph
+    {
+        GraphNode[] Nodes;
+        int count;
+
+        public Graph(int size)
+        {
+            Nodes = new GraphNode[size];
+            count = 0;
+        }
+
+
+    }
+}
