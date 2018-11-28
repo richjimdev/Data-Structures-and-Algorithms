7bfb44ae2f040e2b1431e076c41e883eef8a8cc6
Richard J <richjim89@gmail.com>
2018-11-28 13:32:14 -0800

    delete

--- data-structures/Graphs/Graphs/Classes/Graph.cs
+++ data-structures/Graphs/Graphs/Classes/Graph.cs
@@ -30,5 +30,51 @@ namespace Graphs.Classes
 
             return -1;
         }
+
+        public void AddEdges(string a, string b)
+        {
+            int aIndex = FindNode(a);
+            int bIndex = FindNode(b);
+
+            if (aIndex == -1 || bIndex == -1)
+                Nodes[aIndex].Edges.Add(b);
+
+            Nodes[bIndex].Edges.Add(a);
+        }
+
+        public void Swap(string a, string b)
+        {
+            int aIndex = FindNode(a);
+            int bIndex = FindNode(b);
+
+            GraphNode temp = Nodes[aIndex];
+            Nodes[aIndex] = Nodes[bIndex];
+            Nodes[bIndex] = temp;
+        }
+
+        public void Delete(string x)
+        {
+            int position = FindNode(x);
+            GraphNode[] temp = new GraphNode[Nodes.Length - 1];
+
+            for (int i = 0; i < Nodes.Length; i++)
+            {
+                if (i < position)
+                {
+                    temp[i] = Nodes[i];
+                }
+
+                if (i > position)
+                {
+                    temp[i - 1] = Nodes[i];
+                }
+            }
+
+            DeleteEdge(x);
+            Nodes = temp;
+            count--;
+        }
+
+
     }
 }
