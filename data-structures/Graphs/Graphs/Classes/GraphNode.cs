ea447b785d08041d383d22dada3715ccee5b6e68
Richard J <richjim89@gmail.com>
2018-11-28 13:09:29 -0800

    node

---
+++ data-structures/Graphs/Graphs/Classes/GraphNode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graphs.Classes
+{
+    public class GraphNode
+    {
+        public string Value;
+        public List<string> Edges = new List<string>();
+
+        public GraphNode(string value)
+        {
+            Value = value;
+        }
+    }
+}
