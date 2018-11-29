fe3e57389d9353b911f37c515b87bfce4d04df9e
Richard J <richjim89@gmail.com>
2018-11-28 16:26:22 -0800

    coments

--- data-structures/Graphs/Graphs/Classes/Graph.cs
+++ data-structures/Graphs/Graphs/Classes/Graph.cs
@@ -8,11 +8,27 @@ namespace Graphs.Classes
     {
         public List<GraphNode> Nodes = new List<GraphNode>();
 
-        public void Add(string value)
+        /// <summary>
+        /// Adds a floating node to the graph
+        /// </summary>
+        /// <param name="value">Value of the node to create</param>
+        /// <returns>Confirmation or error message</returns>
+        public string Add(string value)
         {
-            Nodes.Add(new GraphNode(value));
+            if (Find(value) == null)
+            {
+                Nodes.Add(new GraphNode(value));
+                return "Node Added";
+            }
+            else
+                return "Node with that value already exists";
         }
 
+        /// <summary>
+        /// Finds a node in the graph
+        /// </summary>
+        /// <param name="value">Value of node to find</param>
+        /// <returns>Found node, or null if not found</returns>
         private GraphNode Find(string value)
         {
             foreach (GraphNode node in Nodes)
@@ -24,6 +40,13 @@ namespace Graphs.Classes
             return null;
         }
 
+        /// <summary>
+        /// Adds an edge between two existing nodes
+        /// </summary>
+        /// <param name="a">Value of first node</param>
+        /// <param name="b">Value of second node</param>
+        /// <param name="weight">Weight to give to edge</param>
+        /// <returns>Confirmation or error message</returns>
         public string AddEdge(string a, string b, int weight)
         {
             if (Find(a) == null || Find(b) ==null)
@@ -46,16 +69,28 @@ namespace Graphs.Classes
             return "Edge Added";
         }
 
+        /// <summary>
+        /// Adds a node and connects an edge to an existing node
+        /// </summary>
+        /// <param name="parent"></param>
+        /// <param name="node"></param>
+        /// <param name="weight"></param>
+        /// <returns></returns>
         public string Append(string parent, string node, int weight)
         {
             if (Find(parent) == null)
-                return "Parent does no exist";
+                return "Parent does not exist";
             Add(node);
             AddEdge(parent, node, weight);
 
             return "Added node and edge";
         }
 
+        /// <summary>
+        /// Returns all nodes that are connected to input node
+        /// </summary>
+        /// <param name="value">Node to check neighbors off</param>
+        /// <returns>List of neighbor nodes</returns>
         public List<GraphNode> Neighbors(string value)
         {
             GraphNode root = Find(value);
@@ -74,11 +109,19 @@ namespace Graphs.Classes
             return neighbors;
         }
 
+        /// <summary>
+        /// Returns all nodes in graph (can also access using graph.Nodes)
+        /// </summary>
+        /// <returns>List of all nodes</returns>
         public List<GraphNode> GetNodes()
         {
             return Nodes;
         }
 
+        /// <summary>
+        /// Returns the number of nodes in Graph (Can also use graph.Nodes.Count)
+        /// </summary>
+        /// <returns>Number of nodes in graph</returns>
         public int Size()
         {
             return Nodes.Count;
